0ecf1a48876f8b4c2cadb9c8727d3feae5c47330
SusanLiu93 <liuss>
2020-10-28 11:27:50 +0800

    新增移除树节点

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -64,6 +64,55 @@ let BST = (function () {
         return true;
       }
     }
+    /**
+     * 移除节点
+     * 1、找点需要移除的节点
+     * 2、判断该节点的子节点数
+     * 2.1 如果无子节点，直接node 设为null
+     * 2.2 如果有一个左子节点(右子节点)，直接用左(右)子节点替换node
+     * 2.3 如果有左右子节点，首先找出移除节点的右边树的最小节点
+     *     其次 用这个最小节点去替换移除节点
+     *     最后 删除原右边树的最小节点
+     * @param {相对根节点} relativeNode
+     * @param {需要移除的节点} removeNode
+     */
+    remove(relativeNode, removeNode) {
+      if (!relativeNode) {
+        return null;
+      }
+      if (removeNode < relativeNode.data) {
+        relativeNode.left = this.remove(relativeNode.left, removeNode);
+        return relativeNode
+      } else if (removeNode > relativeNode.data) {
+        relativeNode.right = this.remove(relativeNode.right, removeNode);
+        return relativeNode
+      } else {
+        // 找到该节点
+        if (!relativeNode.left && !relativeNode.right) {
+          // 没有子节点
+          relativeNode = null;
+          return relativeNode;
+        }
+        if (!relativeNode.left) {
+          // 如果左子节点为空
+          relativeNode = relativeNode.right;
+          return relativeNode;
+        }
+        if (!relativeNode.right) {
+          // 如果左子节点为空
+          relativeNode = relativeNode.left;
+          return relativeNode;
+        }
+        // 左右子节点都有
+        // 右边树最小的节点
+        let minNode = this.minNode(relativeNode.right);
+        // 替换当前移除节点
+        relativeNode.data = minNode.data;
+        // 删除右边树最小的那个节点
+        relativeNode.right = this.remove(relativeNode.right, minNode.data);
+        return relativeNode;
+      }
+    }
   }
 
   function insertNode(curNode, newNode) {
@@ -112,6 +161,9 @@ let min = tree.minNode(tree.root);
 let max = tree.maxNode(tree.root);
 console.log(min, max);
 
-let isExist = tree.search(tree.root,9);
-let isExist2 = tree.search(tree.root,98);
-console.log(isExist,isExist2);
+let isExist = tree.search(tree.root, 9);
+let isExist2 = tree.search(tree.root, 98);
+console.log(isExist, isExist2);
+
+tree.remove(tree.root, 10);
+tree.inOrder(tree.root);
