259d3497c868f71fe186d7e3b47f27a23cafbaf2
liushanshan <shanshan.liu@datacube.hk>
2020-10-31 10:39:16 +0800

    新增栈min 函数

--- 剑指offer/20.顺时针打印矩阵.js
+++ 剑指offer/20.顺时针打印矩阵.js
@@ -6,3 +6,40 @@
 // 8  10 11 12
 // 13 14 15 16
 // 打印 1、2、3、4、8、12、16、15、14、13、9、5、6、7、11、10
+function printMatrix(numbers, columns, rows) {
+    if (!numbers || columns <= 0 || rows <= 0) {
+        return
+    }
+    let start = 0;
+    while (columns >= start * 2 && rows >= start * 2) {
+        print(numbers, start, columns, rows)
+        start++
+    }
+}
+
+function print(numbers, start, columns, rows) {
+    let endX = columns - 1 - start
+    let endY = rows - 1 - start
+    // 从左到右打印
+    for (let i = start; i <= endX; i++) {
+        console.log(numbers[start][i])
+    }
+    // 从上到下打印
+    if (start < endY) {
+        for (let i = start + 1; i <= endY; i++) {
+            console.log(numbers[i][endX])
+        }
+    }
+    // 从右到左打印
+    if (start < endX && start < endY) {
+        for (let i = endX - 1; i >= start; i--) {
+            console.log(numbers[endY][i])
+        }
+    }
+    // 从下到上打印
+    if (start < endX && start < endY - 1) {
+        for (let i = endY - 1; i >= start + 1; i--) {
+            console.log(numbers[i][start])
+        }
+    }
+}
--- 剑指offer/21.包含min函数的栈.js
+++ 剑指offer/21.包含min函数的栈.js
@@ -2,21 +2,45 @@
  * 定义栈的数据结构， 请在该类型中实现一个能够得到栈的最小元素的min函数。 
  * 在该栈中， 调用min、 push及pop的时间复杂度都是O（ 1）
  */
-
-let obj={
-    get a(){
-        return 1
+function Stack() {
+    this.mData = [] // 先进后出
+    this.mMin = [] // 辅助栈保存最小值，该栈的大小应和数据栈保持一致
+}
+Stack.prototype.push = function (val) {
+    let top=this.top(this.mMin)
+    // 如果添加的值 比最小值小，则正常push
+    if (this.mMin.length === 0 || top>val) {
+        this.mMin.push(val)
+    } else {
+    // 反之，将目前最小的值压入进栈
+        this.mMin.push(top)
+    }
+    this.mData.push(val)
+}
+Stack.prototype.top = function (stack) {
+    if (stack.length > 0) {
+        return stack[stack.length - 1]
+    }
+    return ''
+}
+Stack.prototype.pop = function () {
+    this.mMin.pop()
+   return this.mData.pop()
+}
+Stack.prototype.min = function () {
+    if (this.mMin.length > 0) {
+        return this.mMin[this.mMin.length - 1]
     }
 }
-console.log(obj.a)
-Object.defineProperty(obj,'b',{
-    get(){
-        return this.a*2
-    },
-    set(val){
-        value=val
-    },
-    enumerable:true
-})
-obj.b=3
-console.log(obj.b)
+Stack.prototype.print=function (stack) {
+    console.log(stack.join(','))
+}
+let s=new Stack()
+s.push(6)
+s.push(3)
+s.push(4)
+console.log(s.min())
+s.pop()
+console.log(s.min())
+s.pop()
+console.log(s.min())
