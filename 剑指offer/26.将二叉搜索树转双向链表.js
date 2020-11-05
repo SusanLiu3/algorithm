3871cc560ebe9bbffaa3932adc9f633168f9c213
liushanshan <shanshan.liu@datacube.hk>
2020-11-06 06:57:22 +0800

    树转链表

--- 剑指offer/26.将二叉搜索树转双向链表.js
+++ 剑指offer/26.将二叉搜索树转双向链表.js
@@ -2,3 +2,32 @@
  * 输入一棵二叉搜索树， 将该二叉搜索树转换成一个排序的双向链表。 
  * 要求不能创建任何新的结点， 只能调整树中结点指针的指向。
  */
+
+function treeToLinkLis(root) {
+    if (!root) {
+        return
+    }
+    convert(root, last)
+    while (root.left) {
+        root = root.left
+    }
+    return root
+
+}
+
+function convert(node, last) {
+    if (node.left) {
+        last = convert(node.last) // 遍历到树的最小值
+    }
+    // 找到最小根节点
+    node.left = last
+    if (last) {
+        last.right = node
+    }
+    // 更新last 为node 
+    last = node
+    if (node.right) {
+        last = convert(node.right, last)
+    }
+    return last
+}
