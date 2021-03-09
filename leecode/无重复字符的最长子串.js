73840e58e3ea7ba715f67016121a8a73b05145a8
liushanshan <2498469106@qq.com>
2021-03-09 20:43:40 +0800

    fixed

---
+++ leecode/无重复字符的最长子串.js
@@ -0,0 +1,32 @@
+/**
+ * 给定一个字符串， 请你找出其中不含有重复字符的 最长子串 的长度。
+ */
+
+function lengthOfLongestStr(s) {
+    let max = 0
+    for (let i = 0, j = 0; j < s.length; j++) {
+        let index = s.substring(i, j).indexOf(s[j]) // 判断字符串是否存在和索引j处相同的字符
+        if (index > 0) {
+            i = i + index + 1 ;// 移动第一个指针
+        }
+        max=Math.max(max,j-i+1) // 比较最大值
+    }
+    return max
+}
+/**
+ * 使用map存储值和索引
+ * @param {*} s 
+ */
+function lengthOfLongestStr2(s) {
+        let map =new Map()
+        let max=0
+        for(let i =0,j=0;j<s.length;j++){
+            if (map.has(s[j])){
+                i=Math.max(map.has(s[j])+1,i)
+            }
+            max=Math.max(max,j-i+1)
+            map.set(s[j],j)
+        }
+        return max
+}
+console.log(lengthOfLongestStr2('abcbb'))
